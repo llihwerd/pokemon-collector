849db7e11b41d1b3a818edc1603322dfa1d6bee7
llihwerd <100llihwerd@gmail.com>
2024-02-26 19:00:47 -0600

    finish part 1 of lab

--- main_app/urls.py
+++ main_app/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
   path('', views.home, name='home'),
   path('about/', views.about, name='about'),
+  path('pokemons/', views.pokemon_index, name='pokemon_index'),
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -3,9 +3,27 @@ from django.shortcuts import render
 # Add the following import
 from django.http import HttpResponse
 
+
+class Pokemon:  # Note that parens are optional if not inheriting from another class
+  def __init__(self, name, type, description, hp):
+    self.name = name
+    self.type = type
+    self.description = description
+    self.hp = hp
+
+pokemons = [
+  Pokemon('Bulbasaur', 'grass', 'The cabage frog.', 37),
+  Pokemon('Charmander', 'fire', 'The warm lizard.', 32),
+  Pokemon('Squirtle', 'water', 'The fiesty turle.', 36)
+]
+
+
 # Define the home view
 def home(request):
   return HttpResponse('<h1>Gotta Collect em All</h1>')
 
 def about(request):
   return render(request, 'about.html')
+
+def pokemon_index(request):
+  return render(request, 'pokemons/index.html', { 'pokemons': pokemons })
