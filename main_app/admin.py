a574bc0519f02e447b67a025d2aa852df615f993
llihwerd <100llihwerd@gmail.com>
2024-02-27 22:41:18 -0600

    add data to neondb

---
+++ main_app/admin.py
@@ -0,0 +1,6 @@
+from django.contrib import admin
+# import your models here
+from .models import Pokemon
+
+# Register your models here
+admin.site.register(Pokemon)
--- main_app/views.py
+++ main_app/views.py
@@ -1,21 +1,8 @@
 from django.shortcuts import render
+from .models import Pokemon
 
 # Add the following import
 from django.http import HttpResponse
-
-
-class Pokemon:  # Note that parens are optional if not inheriting from another class
-  def __init__(self, name, type, description, hp):
-    self.name = name
-    self.type = type
-    self.description = description
-    self.hp = hp
-
-pokemons = [
-  Pokemon('Bulbasaur', 'grass', 'The cabage frog.', 37),
-  Pokemon('Charmander', 'fire', 'The warm lizard.', 32),
-  Pokemon('Squirtle', 'water', 'The fiesty turle.', 36)
-]
 
 
 # Define the home view
@@ -26,4 +13,5 @@ def about(request):
   return render(request, 'about.html')
 
 def pokemon_index(request):
+  pokemons = Pokemon.objects.all()
   return render(request, 'pokemons/index.html', { 'pokemons': pokemons })
