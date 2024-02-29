887942f44e77caf46ccb7002d0a84e29ee919ae9
llihwerd <100llihwerd@gmail.com>
2024-02-28 18:32:43 -0600

    add view function

--- main_app/urls.py
+++ main_app/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
   path('', views.home, name='home'),
   path('about/', views.about, name='about'),
   path('pokemons/', views.pokemon_index, name='pokemon_index'),
+  path('pokemons/<int:pokemon_id>/', views.pokemon_detail, name='pokemon-detail'),
 ]
