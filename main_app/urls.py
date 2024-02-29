21652bec38f81c2ff24968c2187ece9e1872237b
llihwerd <100llihwerd@gmail.com>
2024-02-28 19:22:00 -0600

    add pokemon create function

--- main_app/urls.py
+++ main_app/urls.py
@@ -4,7 +4,7 @@ from . import views
 urlpatterns = [
   path('', views.home, name='home'),
   path('about/', views.about, name='about'),
-  path('pokemons/', views.pokemon_index, name='pokemon_index'),
+  path('pokemons/', views.pokemon_index, name='pokemon-index'),
   path('pokemons/<int:pokemon_id>/', views.pokemon_detail, name='pokemon-detail'),
   path('pokemons/create/', views.PokemonCreate.as_view(), name='pokemon-create'),
 ]
