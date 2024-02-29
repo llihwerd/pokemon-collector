b1c8171651e2464b41f0ecf3951e1e0ad1e0ff03
llihwerd <100llihwerd@gmail.com>
2024-02-28 19:49:59 -0600

    add feeding model

--- main_app/models.py
+++ main_app/models.py
@@ -1,6 +1,12 @@
 from django.db import models
 from django.urls import reverse
 
+MEALS = (
+  ('B', 'Berries'),
+  ('P', 'Poffin'),
+  ('M', 'Moo Moo Milk')
+)
+
 class Pokemon(models.Model):
   name = models.CharField(max_length=100)
   type = models.CharField(max_length=100)
@@ -12,3 +18,11 @@ class Pokemon(models.Model):
   
   def get_absolute_url(self):
     return reverse('pokemon-detail', kwargs={'pokemon_id': self.id})
+  
+class Feeding(models.Model):
+  date = models.DateField()
+  meal = models.CharField(
+    max_length=1,
+    choices=MEALS,
+    default=MEALS[0][0]
+  )
