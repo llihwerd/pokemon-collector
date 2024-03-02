8780bee604fdad33fbf91c110309cffa7c052838
llihwerd <100llihwerd@gmail.com>
2024-03-02 12:27:20 -0600

    added owner field to pokemon model

--- main_app/models.py
+++ main_app/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.urls import reverse
 from datetime import date
+from django.contrib.auth.models import User
 
 MEALS = (
   ('B', 'Berries'),
@@ -24,6 +25,7 @@ class Pokemon(models.Model):
   description = models.TextField(max_length=250)
   hp = models.IntegerField()
   toys = models.ManyToManyField(Toy)
+  user = models.ForeignKey(User, on_delete=models.CASCADE)
 
   def __str__(self):
     return self.name
